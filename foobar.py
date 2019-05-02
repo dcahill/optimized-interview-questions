cff86c1b176fa9be6b6c31d08aa03ec16523354a
dcahill <furrious@gmail.com>
2019-05-01 17:27:18 -0700

    changed to for loop

--- foobar.py
+++ foobar.py
@@ -1,8 +1,6 @@
 def foobar(n):
-    
-    count = 1
 
-    while count <= n:
+    for count in range(1, n+1):
         
         div3 = True if count % 3 == 0 else False
         div5 = True if count % 5 == 0 else False
@@ -10,7 +8,6 @@ def foobar(n):
         if div3: print('foo', end="")
         if div5: print ('bar', end="")
         if not div3 and not div5: print(str(count), end="")
-        count += 1
         print()
         
     return
