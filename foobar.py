6f7857f220ce4133c4b4a0e824a45d16a83649a1
dcahill <furrious@gmail.com>
2019-05-01 17:29:58 -0700

    Update foobar.py

--- foobar.py
+++ foobar.py
@@ -1,13 +1,13 @@
 def foobar(n):
 
-    for count in range(1, n+1):
+    for i in range(1, n+1):
         
-        div3 = True if count % 3 == 0 else False
-        div5 = True if count % 5 == 0 else False
+        div3 = True if i % 3 == 0 else False
+        div5 = True if i % 5 == 0 else False
         
         if div3: print('foo', end="")
         if div5: print ('bar', end="")
-        if not div3 and not div5: print(str(count), end="")
+        if not div3 and not div5: print(str(i), end="")
         print()
         
     return
