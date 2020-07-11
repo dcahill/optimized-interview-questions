cf7bcbb14d36174e5252fb8bb21af0698208800f
dcahill <furrious@gmail.com>
2020-07-11 02:49:17 -0700

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,8 +5,8 @@ def foobar(n):
         div3 = True if i % 3 == 0 else False
         div5 = True if i % 5 == 0 else False
         
-        if div3: print('foo', end="")
-        if div5: print('bar', end="")
+        if div3: print('fizz', end="")
+        if div5: print('buzz', end="")
         if not div3 and not div5: print(str(i), end="")
         print()
         
