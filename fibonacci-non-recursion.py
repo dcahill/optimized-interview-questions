5e9834f579bd115a2c849c479e3eb3debde9c74a
dcahill <furrious@gmail.com>
2020-07-11 02:48:10 -0700

    Create fibonacci-non-recursion.py

---
+++ fibonacci-non-recursion.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def fibonacci(number):
+    a = 0
+    b = 1
+    for count in range(number):
+        if count == 0: print('0')
+        elif count == 1: print('1')
+        else:
+            result = a + b
+            print(result)
+            a = b
+            b = result
+
+fibonacci(12)   
+fibonacci(1)
+fibonacci(120)
