65f348a1da2d5d3b987b0c44b8148800e81a8c29
dcahill <furrious@gmail.com>
2020-07-26 18:34:33 -0700

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -6,17 +6,20 @@ def draw_board(cells):
     print('%s | %s | %s' % (cells[6], cells[7], cells[8]))
 
 def check_winner(cells):
+        # Horizontal
     if (all(x==cells[0] for x in cells[0:3]) or
         all(x==cells[3] for x in cells[3:6]) or
         all(x==cells[6] for x in cells[6:9]) or
+        # Vertical
         all(x==cells[0] for x in (cells[0], cells[3], cells[6])) or
         all(x==cells[1] for x in (cells[1], cells[4], cells[7])) or
         all(x==cells[2] for x in (cells[2], cells[5], cells[8])) or
+        # Diagonal
         all(x==cells[0] for x in (cells[0], cells[4], cells[8])) or
         all(x==cells[2] for x in (cells[2], cells[4], cells[6]))
         ):
         return True
-    
+
 # Initialize variables
 player_one = ' X '
 player_two = ' O '
@@ -28,6 +31,9 @@ cells = ['[0]', '[1]', '[2]',
 
 # Main
 while 1:
+    if turn >= 9:
+        print('Tie!')
+        break
     if check_winner(cells):
         print('Winner!')
         break
